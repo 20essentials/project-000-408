bcf5cbacd5d8964b8c4b7ede1dd5c541660fe7e1
DominicDev <s0l0estudi4r@gmail.com>
2025-08-12 12:28:15 +0200

    v0.0.2

---
+++ App.jsx
@@ -0,0 +1,84 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { css, createGlobalStyle } from "@acab/ecsstatic";
+
+createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif, system-ui, -apple-system, BlinkMacSystemFont,
+      'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+      'Helvetica Neue';
+  }
+  a {
+    -webkit-tap-highlight-color: transparent;
+  }
+  body {
+    height: 100vh;
+    width: 100%;
+    overflow: hidden;
+    background-color: #000;
+  }
+  @keyframes rotarColor {
+    0% {
+      --linear-1: 45deg;
+      --linear-2: 135deg;
+    }
+    100% {
+      --linear-1: 405deg;
+      --linear-2: 495deg;
+    }
+  }
+  @property --linear-1 {
+    syntax: "<angle>";
+    inherits: true;
+    initial-value: 45deg;
+  }
+  @property --linear-2 {
+    syntax: "<angle>";
+    inherits: true;
+    initial-value: 135deg;
+  }
+`;
+
+const rectangle = css`
+  position: fixed;
+  inset: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #111;
+  position: relative;
+  background-size: cover;
+  background-position: center;
+  filter: saturate(400%);
+  &::before,
+  &::after {
+    content: '';
+    position: absolute;
+    background: linear-gradient(var(--linear-1), springgreen, violet, cyan),
+      linear-gradient(var(--linear-2), springgreen, violet, cyan);
+    background-blend-mode: hard-light;
+    inset: -5px;
+    z-index: -1;
+    animation: rotarColor 6s ease alternate infinite both;
+  }
+  &::after {
+    filter: blur(30px);
+  }
+`;
+
+function App() {
+  return <aside className={rectangle}></aside>;
+}
+
+const rootEl = document.createElement("div");
+document.body.appendChild(rootEl);
+
+createRoot(rootEl).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
